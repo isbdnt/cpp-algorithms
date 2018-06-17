fd51941e3ffb429593e47c4ea163c48f8541d08a
isbdnt <isbdnt@outlook.com>
2018-06-17 13:03:43 +0800

    update

--- sorting/bubble_sort/bubble_sort.h
+++ sorting/bubble_sort/bubble_sort.h
@@ -1,7 +1,7 @@
 #pragma once
 
 template<typename T>
-void Swap(T& a, T& b)
+void Swap(T &a, T &b)
 {
 	T temp(a);
 	a = b;
--- sorting/bubble_sort/demo.cpp
+++ sorting/bubble_sort/demo.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int arr[] = { 3, 1, 7, 6, 0 };
 
